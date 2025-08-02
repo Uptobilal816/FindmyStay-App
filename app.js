fbcc990e0a3990d6999a7b8a384c636786257199
Muhammad Bilal <bilalms816@gmail.com>
2025-08-02 16:55:49 +0500

    error handler

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express() ;
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
 const path = require("path")
+const methodoverride = require("method-override")
+const ejsMate = require("ejs-mate")
+const expresserror = require("./expresserror.js")
+const wrapAsync = require("./utils/wrapAsync.js")
 
+app.engine('ejs',ejsMate)
 app.set('view engine', "ejs")
 app.set("views",path.join (__dirname,"views"))
 app.use(express.urlencoded({extended : true}));
+app.use(methodoverride("_method"))
+app.use(express.static(path.join(__dirname,"/public")))
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/FindmyStay";
 
@@ -20,6 +27,10 @@ main().then(()=>{
     console.log("Failed to connect to MongoDB");
 });
 
+app.get("/",async (req,res)=>{
+    const alllistings =  await Listing.find({});
+    res.render('listings/index.ejs', {alllistings})
+})
 //Index route
 app.get("/listings", async (req,res)=>{
     const alllistings =  await Listing.find({});
@@ -33,12 +44,13 @@ app.get("/listings/new",(req,res)=>{
 })
 
 //create route
-app.post("/listings",async (req,res)=>{
+app.post("/listings",
+    wrapAsync,(async (req,res,next)=>{
   const newlisting = new Listing(req.body.newlisting)
 await newlisting.save();
 res.redirect("/listings")
 
-})
+}))
 
 //Show route
 app.get("/listings/:id", async (req,res)=>{
@@ -47,6 +59,30 @@ app.get("/listings/:id", async (req,res)=>{
    res.render("listings/show.ejs",{listing});
 })
 
+//Edit route
+app.get("/listings/:id/edit", async (req,res)=>{
+     let {id} = req.params;
+   let listing =  await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+})
+//update route
+app.put("/listings/:id", async (req,res)=>{
+   let {id} = req.params;
+   let updatelisting = await Listing.findByIdAndUpdate(id,{...req.body.newlisting});
+   console.log(updatelisting);
+   res.redirect(`/listings/${id}`);
+})
+//DELETE ROUTE 
+app.delete("/listings/:id", async (req,res)=>{
+let {id} = req.params
+await Listing.findByIdAndDelete(id);
+res.redirect(`/listings`)
+})
+
+app.use((err,req,res,next)=>{
+    res.send("Something went wrong")
+
+})
 // app.get('/testinglist',(req,res)=>{
 //  let samplelisting = new Listing({
 //     title: "Sample Listing",
